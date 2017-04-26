8cc55e91df504a659212422b9c1e530702dfda96
Damix48 <damiano.zanardo@gmail.com>
2017-04-26 23:09:26 +0200

    Add files via upload

--- game_reign/sketch.js
+++ game_reign/sketch.js
@@ -30,26 +30,29 @@ function loadCards() {
 }
 
 function setup() {
-
     noCanvas();
 
     loadCards();
 
     reign = new Reign();
 
+    createP("Reign:");
     reign.show_proprieties();
 
     for (var i = 0; i < common.length; i++) {
         cards.push(new Card(common[i]));
     }
+
     console.log("cards");
     console.log(cards);
+
     for (var i = 0; i < cards.length; i++) {
         console.log("CARD " + (i+1) + " ");
         cards[i].show_proprieties();
     }
 
     reign.applyCard(cards[0], 1);
+
     createP("New Reign:");
     reign.show_proprieties();
 }
