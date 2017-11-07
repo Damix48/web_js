668e9b1526ec2ee9e84fa184cab91c5b28688f8d
Damix48 <damiano.zanardo@gmail.com>
2017-11-07 22:54:01 +0100

    Add color
    Add color, the point aren't synchronized

--- 00_geometry_figures/sketch.js
+++ 00_geometry_figures/sketch.js
@@ -4,18 +4,25 @@ let r;
 let p;
 let target;
 let ball = [];
-let index;
-let number = 10;
+let index = [];
+let number = 13;
 let speed = 3;
 let np = [];
-// let vel;
+let position;
+let c = 0;
+let ll = 1;
+
+let v1;
+let vtarget;
+let vel = [];
 
 function setup() {
   createCanvas(600, 600);
-  // polygon(5);
-
+  a = 0;
 }
 
+let ind = 0;
+let innn = 0;
 
 function draw() {
   background(51);
@@ -23,7 +30,7 @@ function draw() {
   rotate(-PI / 2);
   fill(255);
 
-  rotate(PI);
+  rotate(-PI);
   for (var i = 3; i < number; i++) {
 
     push();
@@ -31,79 +38,34 @@ function draw() {
     pop();
 
   }
-  // for (var i = 0; i < p.length; i++) {
-  //   // var element = array[i];
 
-  // }
-  // for (var i = 3; i < number; i++) {
-  //   // if (frameCount == 1) {
-  //   //   index = 0;
-  //   //   ball.push(p[index]);
-  //   // }
-
-  //   let vel = p5.Vector.sub(p[index + 1], ball[0]);
-  //   vel.setMag(2);
-  //   ball[0].add(vel);
-
-  //   ellipse(ball[0].x, ball[0].y, 10, 10);
-  //   if (p5.Vector.dist(ball[0], p[(index + 1)]) < 1) {
-  //     console.log("uguali");
-  //     console.log(index);
-  //     if (index < i - 3) {
-  //       index++;
-  //     } else {
-  //       index = -1;
-  //     }
-  //   }
-
-  // }
-  if (frameCount == 1) {
-    index = 0;
-    for (let i = 0; i < np.length; i++) {
-      ball.push(np[i][index]);
-    }
-    // ball.push(np[6][index]);
+  points();
+  // console.log(frameRate());
+  // console.log(c);
+  if (c == 255) {
+    l = -1;
+  } else if (c == 0) {
+    l = 1;
   }
-  for (var i = 0; i < ball.length; i++) {
-    push();
-    rotate(PI / (i + 3));
-    let vel = p5.Vector.sub(np[i][(index + 1) % (i + 3)], ball[i]);
-    vel.setMag(speed);
-    ball[i].add(vel);
-
-    ellipse(ball[i].x, ball[i].y, 10, 10);
-    if (p5.Vector.dist(ball[i], np[i][(index + 1) % (i + 3)]) < 0.001) {
-      console.log("uguali");
-      console.log(index);
-      // if (index < i + number) {
-      index++;
-      // } else {
-      //   index = -1;
-      // }
-    }
-
-
-    pop();
-
-  }
+  c = c + l;
+  // c++;
 
 }
 
 function polygon(n) {
-
   rotate(PI / n);
   r = (radius / sin(PI / n)) / 2;
   p = [];
   for (let i = 1; i <= n; i++) {
     // p[n] = [];
-    let x_ = r * cos(TWO_PI * i / n);
-    let y_ = r * sin(TWO_PI * i / n);
+    let x_ = r * cos(TWO_PI * (i - 2) / n);
+    let y_ = r * sin(TWO_PI * (i - 2) / n);
     p.push(createVector(x_, y_));
   }
-  // console.log(p);
 
+  colorMode(HSB);
   strokeWeight(2);
-  stroke(255);
+  stroke(c * 0.9 * map(n, 0, number, 0, 1), 50, 200);
   noFill();
   beginShape();
   for (let i = 0; i < p.length; i++) {
@@ -113,13 +75,37 @@ function polygon(n) {
   endShape(CLOSE);
 
   np[n - 3] = p;
-  // ball(p);
+}
 
+function points() {
 
+  if (frameCount == 1) {
+    for (let i = 0; i < np.length; i++) {
+      index[i] = 0;
+      ball.push(np[i][index[i]]);
+      vel[i] = 0.4;
+    }
+  }
 
-  // for (let i = 0; i < p.length; i++) {
-  //   // console.log(p[i].x);
-  //   let v = p5.Vector.sub(p[i], p[i + 1]);
-  //   ellipse(p[i].x, p[i].y, 10, 10);
-  // }
+  for (var i = 0; i < ball.length; i++) {
+    push()
+    rotate(PI / (i + 3));
+    // console.log(vel[i]);
+    vel[i] = vel[i] + 0.05;
+    if (vel[i] > 1) {
+      vel[i] = 0;
+      index[i] = (index[i] + 1) % [i + 3];
+      ball[i] = np[i][index[i]];
+    }
+    // console.log(index);
+    // console.log(vel);
+    v1 = p5.Vector.lerp(ball[i], np[i][(index[i] + 1) % (i + 3)], vel[i]);
+
+    strokeWeight(2);
+    stroke(255);
+    fill(255, c);
+    ellipse(v1.x, v1.y, 10, 10);
+    pop();
+  }
+
 }
