f1763e7e153bf9e67ef12598d8faff8a79e1a2a7
Damix48 <damiano.zanardo@gmail.com>
2017-11-09 20:37:29 +0100

    Add files via upload
    A variant of 00_geometry_figures

---
+++ 01_lines/sketch.js
@@ -0,0 +1,66 @@
+let radius = 120;
+let number = 16;
+
+let c = 0;
+// let ll = 1;
+
+let a;
+let b;
+
+
+function setup() {
+  createCanvas((number - 1) * 25 * 2 + 40, (number - 3) * 20 + 20);
+  a = [];
+  b = [];
+
+}
+
+function draw() {
+  background(51);
+  translate(20, 20);
+  // rotate(-PI / 2);
+  fill(255);
+
+  // rotate(PI);
+  for (var i = 3; i < number; i++) {
+    push();
+    polygon(i);
+    pop();
+
+  }
+  if (frameCount == 1) {
+    for (var i = 3; i < number; i++) {
+      a[i] = 0;
+      b[i] = 0;
+    }
+  }
+  if (c == 255) {
+    l = -1;
+  } else if (c == 0) {
+    l = 1;
+  }
+  c = c + l;
+}
+
+
+function polygon(n) {
+  // line(0, 0, 10, 10);
+  // r = 10 * (radius / sin(PI / n)) / n;
+  colorMode(HSB);
+  strokeWeight(2);
+  stroke(35 + c * 0.9 * map(n, 0, number, 0, 1), 50, 200);
+  noFill();
+
+  let y_ = n - 3;
+  line(0, y_ * 20, 25 * n * 2, y_ * 20);
+
+  fill(51);
+  if (a[n] > (25 * n * 2)) {
+
+    b[n] = -1;
+  } else if (a[n] <= 0) {
+    b[n] = 1;
+  }
+  a[n] = b[n] * (((number - n) / 2 * (n)) / 5) * frameCount % (25 * n * 2);
+  ellipse(a[n], y_ * 20, 10, 10);
+}
