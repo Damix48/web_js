59926adb4c8b04816aa03f9e9418772f974a557e
Damix48 <damiano.zanardo@gmail.com>
2018-12-11 00:48:45 +0100

    add map and cell class

--- 06_fredkin_replicator/sketch.js
+++ 06_fredkin_replicator/sketch.js
@@ -1,9 +1,60 @@
+let mappa;
+
 function setup() {
   createCanvas(800, 800);
+  mappa = new Mappa(80, 80);
 }
 
 function draw() {
   background(51);
-  ellipseMode(CENTER);
-  ellipse(10, 10, 10, 10);
+  mappa.show();
+}
+
+class Mappa {
+  constructor(col_, row_) {
+    this.cols = col_;
+    this.rows = row_;
+    this.w = floor(width / this.cols);
+    this.h = floor(height / this.rows);
+    this.cells = this.initCells();
+  }
+
+  initCells() {
+    let temp = [];
+    for (let i = 0; i < this.cols; i++) {
+      temp[i] = [];
+      for (let j = 0; j < this.rows; j++) {
+        temp[i][j] = new Cell(j * this.w, i * this.h);
+      }
+    }
+    return temp;
+  }
+
+  show() {
+    this.cells.forEach(row => {
+      row.forEach(cell => {
+        cell.show();
+      });
+    });
+  }
+}
+
+class Cell {
+  constructor(x_, y_) {
+    this.x = x_;
+    this.y = y_;
+
+    this.active = false;
+  }
+
+  show() {
+    strokeWeight(1);
+    stroke(255);
+    if (this.active) {
+      fill(255);
+    } else {
+      noFill();
+    }
+    rect(this.x, this.y, mappa.w, mappa.h);
+  }
 }
