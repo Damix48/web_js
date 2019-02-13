c027f6af962149ff91400b46ea53d6f5ec686fe7
Damiano Zanardo <damiano.zanardo@gmail.com>
2019-02-14 00:37:49 +0100

    add 08_chaos_triangles

---
+++ 08_chaos_triangles/README.md
@@ -0,0 +1,3 @@
+# 08_chaos_triangles
+
+click [here](https://damix48.github.io/web_js/08_chaos_triangles) to go to the web page
---
+++ 08_chaos_triangles/sketch.js
@@ -0,0 +1,53 @@
+let points = [];
+let last_point;
+
+function setup() {
+  createCanvas(800, 800);
+
+  // SETUP TRIANGLE
+  for (let i = 0; i < 3; i++) {
+    let x = 300 * cos(TWO_PI * i / 3 - PI / 2);
+    let y = 300 * sin(TWO_PI * i / 3 - PI / 2);
+    points.push(createVector(x, y));
+  }
+
+  // ADD FIRST POINT
+  let x = lerp(points[0].x, points[1].x, 0.5);
+  let y = lerp(points[0].y, points[1].y, 0.5);
+  points.push(createVector(x, y));
+}
+
+function draw() {
+  background(51);
+
+  // ADD POINTS
+  for (let i = 0; i < 100; i++) {
+    let p = p5.Vector.lerp(points[points.length - 1], points[floor(random(3))], 0.5);
+    points.push(p);
+  }
+
+  // DRAW ALL-1 POINTS
+  translate(width / 2, height / 2);
+  for (let i = 0; i < points.length - 1; i++) {
+    const p = points[i];
+
+    stroke(255);
+    strokeWeight(1);
+    point(p.x, p.y);
+  }
+
+  // DRAW LAST POINT
+  stroke(255, 10, 10);
+  strokeWeight(10);
+  const p = points[points.length - 1];
+  point(p.x, p.y);
+
+  // TEXT
+  translate(-width / 2, -height / 2);
+  noStroke();
+  fill(255);
+  textAlign(RIGHT);
+  text("POINTS: " + points.length, width - 50, height - 50);
+
+
+}
