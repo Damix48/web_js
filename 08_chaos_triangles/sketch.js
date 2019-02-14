add6013cdaf4ae52bdd602400fceb08110eb3f58
Damiano Zanardo <damiano.zanardo@gmail.com>
2019-02-14 12:28:00 +0100

    update and add 09

--- 08_chaos_triangles/sketch.js
+++ 08_chaos_triangles/sketch.js
@@ -14,8 +14,11 @@ function setup() {
   // ADD FIRST POINT
   let x = lerp(points[0].x, points[1].x, 0.5);
   let y = lerp(points[0].y, points[1].y, 0.5);
+  x = random(800);
+  y = random(800);
   points.push(createVector(x, y));
 }
+let n = 1;
 
 function draw() {
   background(51);
@@ -28,11 +31,13 @@ function draw() {
 
   // DRAW ALL-1 POINTS
   translate(width / 2, height / 2);
+  // scale(n);
   for (let i = 0; i < points.length - 1; i++) {
     const p = points[i];
 
     stroke(255);
     strokeWeight(1);
+    // set(floor(p.x), floor(p.y), color(255));
     point(p.x, p.y);
   }
 
@@ -48,6 +53,8 @@ function draw() {
   fill(255);
   textAlign(RIGHT);
   text("POINTS: " + points.length, width - 50, height - 50);
-
-
+  // n = n + 0.05;
+  if (points.length > 100000) {
+    noLoop();
+  }
 }
---
+++ 09_generic_times_table/README.md
@@ -0,0 +1,3 @@
+# 09_generic_times_table
+
+click [here](https://damix48.github.io/web_js/09_generic_times_table) to go to the web page
