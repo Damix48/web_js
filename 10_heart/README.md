cc2c4e78e55e6adb3b3ab8e6074c3ae7b675755b
Damiano Zanardo <damiano.zanardo@gmail.com>
2019-02-14 19:10:40 +0100

    add heart

---
+++ 10_heart/README.md
@@ -0,0 +1,3 @@
+# 10_heart
+
+click [here](https://damix48.github.io/web_js/10_heart) to go to the web page
---
+++ 10_heart/sketch.js
@@ -0,0 +1,41 @@
+// (x²+y²+1)³-x²y³=0
+let points = [];
+let n = 0;
+
+
+function setup() {
+  createCanvas(800, 800);
+  for (let i = 0; i < TWO_PI; i += 0.05) {
+    let x = 16 * pow(sin(i), 3);
+    let y = 13 * cos(i) - 5 * cos(2 * i) - 2 * cos(3 * i) - cos(4 * i);
+
+    points.push(createVector(20 * x, 20 * y));
+
+  }
+  frameRate(1);
+}
+
+function draw() {
+  background(51);
+  translate(width / 2, height / 2 - 70);
+  rotate(PI)
+
+  for (let i = 0; i < points.length; i++) {
+    const p = points[i];
+    const p_1 = points[(i + 1) % points.length];
+    const p_next = points[floor(i * n) % points.length];
+
+    stroke(255, 50);
+    strokeWeight(2);
+    line(p.x, p.y, p_next.x, p_next.y);
+
+    stroke(255, 111, 97);
+    strokeWeight(10);
+    point(p.x, p.y);
+    strokeWeight(5);
+    line(p.x, p.y, p_1.x, p_1.y);
+
+  }
+
+  n++;
+}
